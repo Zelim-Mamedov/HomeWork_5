8138637f54b572399ddf25085b9a97e019433145
Zelim <zelya140489@gmail.com>
2023-02-02 00:29:21 +0600

    Создали и изменили программы

---
+++ Task36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = CreateArrayRndInt(4, -99, 99);
+PrintArray(array);
+int sumOddNum = SumOfOddNumbers(array);
+Console.WriteLine($" сумма элементов стоящих на нечетных позициях -> {sumOddNum}");
+
+int SumOfOddNumbers(int[] arr)
+{
+    int sumOddNum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ((i + 1) % 2 == 0)
+            sumOddNum += array[i];
+    }
+    return sumOddNum;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
---
+++ Task38/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] array = CreateArrayRndDouble(5, 1, 100);
+PrintArray(array);
+
+double maxNumber = MaxNumber(array);
+double minNumber = MinNumber(array);
+double difference = Difference(maxNumber, minNumber);
+
+Console.WriteLine($" -> {Math.Round(difference, 1, MidpointRounding.ToZero)}");
+Console.WriteLine($"{maxNumber} - {minNumber}"); // Для проверки! Хотел посмотреть правильно ли получаю числа и разницу!
+
+double Difference(double maxNumber, double minNumber)
+{
+    double difference = maxNumber - minNumber;
+    return difference;
+}
+
+double MinNumber(double[] arr)
+{
+    double minNumber = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < minNumber )
+        minNumber = arr[i];
+    }
+    return minNumber;
+}
+
+double MaxNumber(double[] arr)
+{
+    double maxNumber = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > maxNumber )
+        maxNumber = arr[i];
+    }
+    return maxNumber;
+}
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{Math.Round(array[i], 1, MidpointRounding.ToZero)} | "); 
+        }
+        else Console.Write($"{Math.Round(array[i], 1, MidpointRounding.ToZero)}]");
+    }
+}
